d425e8dd9dd733d4e5af5896234c8ec8195729c7
tarushi29 <128239884+tarushi29@users.noreply.github.com>
2023-03-23 13:41:57 +0530

    Add files via upload

---
+++ BILLINGSYSTEM.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+class Item {
+    private:
+        string name;
+        int quantity;
+        double price;
+    public:
+        Item(string n="", int q=0, double p=0.0) {
+            name = n;
+            quantity = q;
+            price = p;
+        }
+
+        string getName() {
+            return name;
+        }
+
+        int getQuantity() {
+            return quantity;
+        }
+
+        double getPrice() {
+            return price;
+        }
+
+        void setName(string n) {
+            name = n;
+        }
+
+        void setQuantity(int q) {
+            quantity = q;
+        }
+
+        void setPrice(double p) {
+            price = p;
+        }
+};
+
+class Bill {
+    private:
+        string customerName;
+        string customerContact;
+        Item *items;
+        int itemCount;
+    public:
+        Bill(string name="", string contact="", int itemCount=0) {
+            customerName = name;
+            customerContact = contact;
+            this->itemCount = itemCount;
+            items = new Item[itemCount];
+        }
+
+        string getCustomerName() {
+            return customerName;
+        }
+
+        string getCustomerContact() {
+            return customerContact;
+        }
+
+        int getItemCount() {
+            return itemCount;
+        }
+
+        Item getItem(int index) {
+            return items[index];
+        }
+
+        void setCustomerName(string name) {
+            customerName = name;
+        }
+
+        void setCustomerContact(string contact) {
+            customerContact = contact;
+        }
+
+        void setItemCount(int count) {
+            itemCount = count;
+            items = new Item[itemCount];
+        }
+
+        void setItem(int index, Item item) {
+            items[index] = item;
+        }
+
+        double getTotal() {
+            double total = 0.0;
+            for(int i = 0; i < itemCount; i++) {
+                total += items[i].getPrice() * items[i].getQuantity();
+            }
+            return total;
+        }
+};
+
+void displayBill(Bill bill) {
+    cout << "Customer Name: " << bill.getCustomerName() << endl;
+    cout << "Customer Contact: " << bill.getCustomerContact() << endl;
+    cout << setw(15) << left << "Item Name" << setw(10) << left << "Quantity" << setw(10) << left << "Price" << endl;
+    for(int i = 0; i < bill.getItemCount(); i++) {
+        Item item = bill.getItem(i);
+        cout << setw(15) << left << item.getName() << setw(10) << left << item.getQuantity() << setw(10) << left << item.getPrice() << endl;
+    }
+    cout << "Total: " << bill.getTotal() << endl;
+}
+
+void saveBill(Bill bill) {
+    ofstream file("bill.txt");
+    file << "Customer Name: " << bill.getCustomerName() << endl;
+    file << "Customer Contact: " << bill.getCustomerContact() << endl;
+    file << setw(15) << left << "Item Name" << setw(10) << left << "Quantity" << setw(10) << left << "Price" << endl;
+    for(int i = 0; i < bill.getItemCount(); i++) {
+        Item item = bill.getItem(i);
+        file << setw(15) << left << item.getName() << setw(10) << left << item.getQuantity() << setw(10) << left << item.getPrice() << endl;
+    }
+    file << "Total: " << bill.getTotal() << endl;
+    file.close();
+
+}
+
+
+
+
